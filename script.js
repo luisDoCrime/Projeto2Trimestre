cad90d6ceaa2e44400f0e895839187e7d5eb0a21
Luis Gustavo <112082405+luisDoCrime@users.noreply.github.com>
2024-07-25 16:36:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resultado");
+
