eaf8b7b13cd642c8f56353931cccb2498598f5df
Luis Gustavo <112082405+luisDoCrime@users.noreply.github.com>
2024-08-15 16:25:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,44 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-resultados");
 const textoResultado = document.querySelector(".texto-resultado");
+const perguntas [
+    {
+        enunciado:"A mudança climática é causada principalmente por?",
+        alternativa:[
+            "Atividades humanas."
+            "Ciclos naturais da Terra."
+        ]
+    }
 
+    {
+        enunciado:"Qual é uma fonte de energia renovável?",
+        alternativa:[
+            "Energia solar."
+            "Carvão."
+        ]
+    }
+
+    {
+        enunciado:"O que pode ocorrer devido ao desmatamento?",
+        alternativa:[
+            "Perda de biodiversidade"
+            "Aumento da fertilidade do solo."
+        ]
+    }
+
+    {
+        enunciado:"A poluição dos oceanos pode afetar?",
+        alternativa:[
+            "A vida marinha."
+            "A qualidade do ar em áreas urbanas."
+        ]
+    }
+
+    {
+        enunciado:"Qual das seguintes ações ajuda a reduzir a pegada de carbono?",
+        alternativa:[
+            "Usar transporte público."
+            "Aumentar o uso de combustíveis fósseis."
+        ]
+    }
+]
